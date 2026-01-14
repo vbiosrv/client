2e147dde2cf101c8074fca0335df9f5804a9d7f7
Daniil Firsov <daniil.firsov@pimpay.ru>
2026-01-14 15:34:33 +0300

    dnk: User telegram login: Используем отдельный новый метод

--- app/scripts/shm/user.js
+++ app/scripts/shm/user.js
@@ -7,6 +7,8 @@ angular
 
     shm_request('GET','v1/user' ).then(function(response) {
         $scope.data = response.data.data[0];
+        // Загружаем telegram username отдельно
+        $scope.loadTelegramUsername();
     });
 
     $scope.partnerUrlToClipboard = function(string) {
@@ -14,10 +16,45 @@ angular
     }
 
     $scope.save = function() {
-        shm_request('POST_JSON','v1/user', $scope.data ).then(function(response) {
-            $scope.data = response.data.data[0];
-            $location.path('/user');
-        })
+        // Сначала сохраняем telegram username, затем основные данные
+        $scope.saveTelegramUsername().then(function() {
+            // Создаем копию данных без telegram настроек для основного сохранения
+            var dataToSave = angular.copy($scope.data);
+            if (dataToSave.settings && dataToSave.settings.telegram) {
+                delete dataToSave.settings.telegram;
+            }
+
+            shm_request('POST_JSON','v1/user', dataToSave ).then(function(response) {
+                $scope.data = response.data.data[0];
+                // Восстанавливаем telegram данные после основного сохранения
+                $scope.loadTelegramUsername();
+                $location.path('/user');
+            })
+        });
+    }
+
+    $scope.saveTelegramUsername = function() {
+        if ($scope.data.settings && $scope.data.settings.telegram &&
+            $scope.data.settings.telegram.username !== null &&
+            $scope.data.settings.telegram.username !== undefined) {
+            var payload = {
+                username: $scope.data.settings.telegram.username || ""
+            };
+            return shm_request('POST_JSON','v1/telegram/user', payload );
+        }
+        return Promise.resolve();
+    }
+
+    $scope.loadTelegramUsername = function() {
+        shm_request('GET','v1/telegram/user' ).then(function(telegramResponse) {
+            if (!$scope.data.settings) {
+                $scope.data.settings = {};
+            }
+            if (!$scope.data.settings.telegram) {
+                $scope.data.settings.telegram = {};
+            }
+            $scope.data.settings.telegram.username = telegramResponse.data.username;
+        });
     }
 
     $scope.passwd = function () {
